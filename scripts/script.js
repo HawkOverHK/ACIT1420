0430a13a02d47aafb05f94d43756280bcdf57082
Norvin <chnorvin@gmail.com>
2022-11-10 19:15:50 -0800

    Start of the Checks

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-let CourseList = {
+const CourseList = {
     code: "COMM 1116", name: "Business Communications",
     code: "ACIT 1630", name: "Database Systems",
     code: "MATH 1310", name: "Technical Math"
@@ -8,8 +8,15 @@ let CourseList = {
 
 let input;
 do{
-input = Number(prompt ("Please enter a 4 digit pin"));
+input = prompt ("Please enter a 4 digit pin");
 //prompt always returns string
 //convert input to number
 // console.log(Number(input)); removed from previous activity
-} while(input !== 1116 && input !== 1630 && input !== 1310)
+} while(input !== '1116' && input !== '1630' && input !== '1310');
+
+for( 
+    let code in CourseList
+)
+{
+    if(input === "code")
+}
